0ee7090beb217e29e38ace0b7f530889bbd9fd86
ClayMaxwell <73453094+ClayMaxwell@users.noreply.github.com>
2020-10-25 22:46:21 -0500

    Week 6
    Added week 6

--- Project #1.py
+++ Project #1.py
@@ -1,8 +1,11 @@
-A = int(input("First side length: "))
-B = int(input("Second side length: "))
-C = int(input("Third side length: "))
-
-if A == B == C:
-    print("The triangle is equalateral")
-else:
-    print("The triangle is not equalateral")
+Text = input("Enter text to encrypt: ")
+distance = int(input("Enter number of offset: "))
+encrypted = ""
+for ch in Text:
+    Value = ord(ch)
+    EnVal = Value + distance
+    if EnVal > ord('z'):
+        EnVal = ord('a') + offset - \
+            (ord('z') - Value + 1)
+    encrypted = encrypted + chr(EnVal)
+print(encrypted)
---
+++ payroll.py
@@ -0,0 +1,5 @@
+LastName  HourlyWage  HoursWorked
+Robins      $12.00         40 
+Daniels     $15.00         25 
+Martinez    $12.00         34 
+Palmer      $14.50         38 
