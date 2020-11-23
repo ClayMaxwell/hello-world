8dddb0538151ad73d6a6e1e7786045e05596499d
ClayMaxwell <73453094+ClayMaxwell@users.noreply.github.com>
2020-11-22 23:30:59 -0600

    Add files via upload

--- Project #5.py
+++ Project #5.py
@@ -1,11 +1,17 @@
-Organisms = int(input("How many organisms are there: "))
-RateOfGrowth = int(input("What is the growth rate (a real number greater than 0): "))
-NumberOfHours = int(input("How many hours are needed to achieve growth rate: "))
-TotalHours = int(input("How many hours of growth were there: "))
-Hours=0
-while (Hours <= TotalHours):
-    Organisms*=RateOfGrowth
-    Hours += NumberOfHours
-    if (Hours==TotalHours):
-        break
-print("The total organism population is",Organisms)
+def isSorted(number):
+     if len(number) >= 0 and len(number) < 2:
+          return True
+     else:
+          if len(number) < 0 and len(number) > 2:
+               return False
+def main():
+     number = [23]
+     print(isSorted(number))
+     number = [14]
+     print(isSorted(number))   
+     number = [-88]   
+     print(isSorted(number))
+     number = [6]  
+     print(isSorted(number))
+
+main()
